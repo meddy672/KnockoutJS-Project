d2b1ed3471adc424691e90b9a70568fa5bf75166
Matthew Eddy <meddy672@gmail.com>
2018-04-21 10:58:37 -0400

    Updated Project

--- js/app.js
+++ js/app.js
@@ -4,18 +4,65 @@
 $(document).ready(function(){
 
 
-	var Cat = function(){
+	var cats = [
 
-		this.clickCount = ko.observable(0)
-		this.name = ko.observable('Bernard')
-		this.imgSrc = ko.observable('public/assets/images/Bernard.jpg'),
-		this.nicknames = ko.observableArray([{ nickname:'Cornel'}, { nickname:'Jeffery'}, { nickname:'Cash'}, {nickname:'Abbey'}]);
+			{
+
+				clickCount: 0,
+				name: 'Bernard',
+				nicknames: [{nickname:'Cornel'}, {nickname:'Jeffery'}, {nickname:'Cash'}],
+				imgSrc: 'public/assets/images/Bernard.jpg',
+				imgAttr: 'Creative Commons',
+				
+			},
+			{
+
+				clickCount: 0,
+				name: 'Melson',
+				nicknames: [{nickname:'Burno'}],
+				imgSrc: 'public/assets/images/Melson.jpg',
+				imgAttr: 'Creative Commons',
+			},
+			{
+
+				clickCount: 0,
+				name: 'SnowFlake',
+				nicknames: [{nickname:'Cannon'}],
+				imgSrc: 'public/assets/images/SnowFlake.jpg',
+				imgAttr: 'Creative Commons',
+			},
+			{
+
+				clickCount: 0,
+				name: 'Willow',
+				nicknames: [{nickname:'Zzzzz'}],
+				imgSrc: 'public/assets/images/Willow.jpg',
+				imgAttr: 'Creative Commons'
+			},
+			{
+
+				clickCount: 0,
+				name: 'Wilson',
+				nicknames: [{nickname:'Richard'}],
+				imgSrc: 'public/assets/images/Wilson.jpg',
+				imgAttr: 'Creative Commons',
+			}
+
+		];
+
+	var Cat = function(cats){
+
+		this.clickCount = ko.observable(cats.clickCount)
+		this.name = ko.observable(cats.name)
+		this.imgSrc = ko.observable(cats.imgSrc),
+		this.nicknames = ko.observableArray(cats.nicknames);
 		this.imgAttr = ko.observable('Creative Commons')
 
+		var viewModel = this;
 		this.title = ko.computed(function(){
 
 			var title = "";
-			var clicks = this.clickCount();
+			var clicks = viewModel.clickCount();
 
 			if(clicks < 10){
 				title = 'Newborn';
@@ -43,19 +90,34 @@ $(document).ready(function(){
 
 			return title;
 
-		}, this)
+		})
 			
 	};
 
 
 
 	var viewModel = function() {
+
+		var self = this;
 		
-		this.currentCat = ko.observable(new Cat());
+		this.catList = ko.observableArray([]);
+
+		cats.forEach(function(cats){
+
+			
+			self.catList.push( new Cat(cats) );
+		})
+		console.log(cats[0])
+		this.currentCat = ko.observable( this.catList()[0] );
 
 		this.incrementCounter = function(){
 
-			this.currentCat().clickCount(this.currentCat().clickCount()+1)
+			self.currentCat().clickCount(self.currentCat().clickCount()+1);
+		};
+
+		this.updateMainImg = function(data){
+
+			self.currentCat(data);
 		}
 
 		
